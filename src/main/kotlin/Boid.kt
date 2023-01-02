a89293b71f4f9d90561b83d6224a6eaa139f4fcf
Gabe Truitt <gabe.truitt@charthop.com>
2023-01-02 00:37:50 -0500

    use Doubles everywhere, complete heading change strategy

--- src/main/kotlin/Boid.kt
+++ src/main/kotlin/Boid.kt
@@ -1,21 +1,24 @@
-import java.util.UUID
+import org.openrndr.math.Vector2
+import java.util.*
 import kotlin.random.Random
 
 data class Boid(
-    val x: Int,
-    val y: Int,
+    val location: Vector2,
     val heading: Double,
     val speed: Double,
     val id: UUID = UUID.randomUUID()
 ) {
     companion object {
         fun randomBoid() = Boid(
-            x = Random.nextInt(Config.SCREEN_SIZE_X.value.inc()),
-            y = Random.nextInt(Config.SCREEN_SIZE_Y.value.inc()),
-            heading = Random.nextDouble(Constants.TWO_PI),
+            location = Vector2(
+                Random.nextDouble(Config.SCREEN_SIZE_X.value.toDouble()),
+                Random.nextDouble(Config.SCREEN_SIZE_Y.value.toDouble())
+            ),
+            heading = Random.nextDouble(TWO_PI),
             speed = Random.nextDouble(
                 Config.BOID_MIN_SPEED.value.toDouble(),
-                Config.BOID_MAX_SPEED.value.toDouble())
+                Config.BOID_MAX_SPEED.value.toDouble()
+            )
         )
     }
 }
